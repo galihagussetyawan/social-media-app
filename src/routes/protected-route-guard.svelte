1ed038385de96eb8725f83d272bbcf854f5a1e91
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-17 16:10:44 +0700

    add: protec route guard function

---
+++ src/routes/protected-route-guard.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { useNavigate, useLocation } from "svelte-navigator";
+  import { currentUser } from "../stores/auth.store";
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  $: if (!$currentUser) {
+    navigate("/register", {
+      state: { from: $location.pathname },
+      replace: true,
+    });
+  }
+</script>
+
+{#if $currentUser}
+  <slot />
+{/if}
--- src/routes/protected-route.svelte
+++ src/routes/protected-route.svelte
@@ -1,25 +1,12 @@
 <script defer>
-  import { onMount } from "svelte";
   import { Route } from "svelte-navigator";
-  import { currentUser } from "../stores/auth.store";
+  import PrivateRouteGuard from "./protected-route-guard.svelte";
 
-  let isLoading = true;
   export let path;
-  export let component;
-
-  currentUser.subscribe(async (value) => {
-    setTimeout(() => {
-      isLoading = false;
-    }, 3000);
-  });
 </script>
 
-{#if isLoading}
-  <div class=" w-full h-screen flex justify-center items-center">
-    Loading.....
-  </div>
-{:else if !$currentUser}
-  <div>Login</div>
-{:else}
-  <Route {path} {component} />
-{/if}
+<Route {path} let:params let:location let:navigate>
+  <PrivateRouteGuard>
+    <slot {params} {location} {navigate} />
+  </PrivateRouteGuard>
+</Route>
