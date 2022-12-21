4262463a28ed783e2badf10a9c08c10b75f50429
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-22 02:50:45 +0700

    change: return value on function getUserByUsername

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,7 +3,10 @@ import {  collection, getDocs, query, where } from "firebase/firestore";
 
 export async function getUserByUsername(username) {
     const userSnap = await getDocs(query(collection(db, 'users'), where('username', '==', username)));
-    const userResult = userSnap.docs.find(docSnap => docSnap?.data()?.username === username).data();
+    const userResult = userSnap.docs.find(docSnap => docSnap?.data()?.username === username);
 
-    return userResult;
+    return {
+        id: userResult.id,
+        ...userResult.data(),
+    }
 }
