7de01bec62e4ade56c54ae6b45f4e401ecb2b084
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-22 02:37:02 +0700

    add: profile service

---
+++ src/services/profile.service.js
@@ -0,0 +1,24 @@
+import { db } from "../config/firebase";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { currentUser } from "../stores/auth.store";
+
+async function getProfileByUserId(userId) {
+
+    try {
+        const profileSnap = await getDocs(query(collection(db, 'users', userId, 'profile'), where('userId', '==', userId)));
+
+        return profileSnap.docs.map(docSnap => docSnap.data());
+    } catch (error) {
+
+    }
+}
+
+async function addProfile(bio, job, city) {
+    currentUser.subscribe(async userValue => {
+        if(userValue?.uid) {
+            await addDoc(collection(db, 'users', userValue?.uid, 'profiles'), {
+                bio, job, city
+            })
+        }
+    })
+}
