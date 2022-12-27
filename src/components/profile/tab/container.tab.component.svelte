f7955f3852015690dddc920394928a80464848c8
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-28 01:53:10 +0700

    add feed card list on tab profile

--- src/components/profile/tab/container.tab.component.svelte
+++ src/components/profile/tab/container.tab.component.svelte
@@ -1,7 +1,17 @@
 <script defer>
+  import { tabProfile } from "../../../stores/global.store";
   import ButtonTab from "./button.tab.component.svelte";
+  import PostsTab from "./posts.tab.component.svelte";
 </script>
 
-<div class="m-auto mt-10 py-5 border-t border-gray-300">
+<div class=" grid space-y-5 m-auto mt-10 py-5 border-t border-gray-300">
   <ButtonTab />
+
+  {#if $tabProfile === "posts"}
+    <svelte:component this={PostsTab} />
+  {:else if $tabProfile === "replies"}
+    <div>Replies</div>
+  {:else if $tabProfile === "tags"}
+    <div>Tags</div>
+  {/if}
 </div>
--- src/components/profile/tab/posts.tab.component.svelte
+++ src/components/profile/tab/posts.tab.component.svelte
@@ -1,1 +1,20 @@
-<div>tab posts</div>
+<script defer>
+  import { onMount } from "svelte";
+  import { currentUser } from "../../../stores/auth.store";
+  import { getFeedsByUserId } from "../../../services/feed.service";
+  import FeedCard from "../../feed/feed-card.component.svelte";
+
+  let isLoading = true;
+  let datas = [];
+
+  onMount(async () => {
+    datas = await getFeedsByUserId($currentUser?.uid);
+    isLoading = false;
+  });
+</script>
+
+<div class="space-y-5">
+  {#each datas as data}
+    <svelte:component this={FeedCard} {data} />
+  {/each}
+</div>
