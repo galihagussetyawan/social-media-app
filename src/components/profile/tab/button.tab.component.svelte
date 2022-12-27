d2d22feb39f1ea40c7da4c49cdc1bc9df23c0b9a
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-27 16:38:20 +0700

    add styling active tab

--- src/components/profile/tab/button.tab.component.svelte
+++ src/components/profile/tab/button.tab.component.svelte
@@ -1,7 +1,34 @@
+<script defer>
+  import { tabProfile } from "../../../stores/global.store";
+
+  function handleChangeTab(tab) {
+    tabProfile.set(tab);
+  }
+
+  function activeTabClass(tab) {
+    return $tabProfile === tab
+      ? "text-black border-2 border-gray-300 bg-white"
+      : "text-gray-500";
+  }
+</script>
+
 <div class="grid grid-cols-3 mx-5 rounded-2xl bg-gray-300">
-  <button class="py-2 rounded-2xl text-black border-2 border-gray-300 bg-white"
-    >Posts</button
+  <button
+    class={$tabProfile === "posts"
+      ? "py-2 rounded-2xl text-black border-2 border-gray-300 bg-white"
+      : "py-2 rounded-2xl text-gray-500"}
+    on:click={() => handleChangeTab("posts")}>Posts</button
   >
-  <button class="py-2 rounded-2xl text-gray-500">Replies</button>
-  <button class="py-2 rounded-2xl text-gray-500">Tags</button>
+  <button
+    class={$tabProfile === "replies"
+      ? "py-2 rounded-2xl text-black border-2 border-gray-300 bg-white"
+      : "py-2 rounded-2xl text-gray-500"}
+    on:click={() => handleChangeTab("replies")}>Replies</button
+  >
+  <button
+    class={$tabProfile === "tags"
+      ? "py-2 rounded-2xl text-black border-2 border-gray-300 bg-white"
+      : "py-2 rounded-2xl text-gray-500"}
+    on:click={() => handleChangeTab("tags")}>Tags</button
+  >
 </div>
