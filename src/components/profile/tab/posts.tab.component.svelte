a44d19c3c8b50f67915d8bcde3e81ff15d02dd70
galihagussetyawan <galihdevelopment@gmail.com>
2022-12-28 02:14:54 +0700

    improve set feeds data on global store

--- src/components/profile/tab/posts.tab.component.svelte
+++ src/components/profile/tab/posts.tab.component.svelte
@@ -2,14 +2,16 @@
   import { onMount } from "svelte";
   import { currentUser } from "../../../stores/auth.store";
   import { getFeedsByUserId } from "../../../services/feed.service";
+  import { userFeedsData } from "../../../stores/feed.store";
   import FeedCard from "../../feed/feed-card.component.svelte";
   import CircleSpinner from "../../skeleton/circle-spinner.component.svelte";
 
   let isLoading = true;
-  let datas = [];
 
   onMount(async () => {
-    datas = await getFeedsByUserId($currentUser?.uid);
+    if (!$userFeedsData) {
+      userFeedsData.set(await getFeedsByUserId($currentUser?.uid));
+    }
     isLoading = false;
   });
 </script>
@@ -18,7 +20,7 @@
   {#if isLoading}
     <svelte:component this={CircleSpinner} />
   {:else}
-    {#each datas as data}
+    {#each $userFeedsData as data}
       <svelte:component this={FeedCard} {data} />
     {/each}
   {/if}
--- src/stores/feed.store.js
+++ src/stores/feed.store.js
@@ -1,2 +1,3 @@
 import { writable } from "svelte/store";
 export const feedsData = writable();
+export const userFeedsData = writable();
