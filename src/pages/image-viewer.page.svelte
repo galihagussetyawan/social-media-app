8d46bd109b65cea7784d08fb38d11d46a9afbb98
galihagussetyawan <galihdevelopment@gmail.com>
2023-01-11 02:19:48 +0700

    feat: add navigate between images

--- src/pages/image-viewer.page.svelte
+++ src/pages/image-viewer.page.svelte
@@ -1,7 +1,7 @@
 <script defer>
   import { onMount } from "svelte";
   import { LazyImage } from "svelte-lazy-image";
-  import { useLocation, useParams } from "svelte-navigator";
+  import { navigate, useLocation, useParams } from "svelte-navigator";
   import { getFeedById } from "../services/feed.service";
 
   const location = useLocation();
@@ -14,12 +14,35 @@
     if (!$location?.state) {
       imagesList = await (await getFeedById($params?.feedid))?.images;
     } else {
-      imagesList = Object.keys($location?.state)?.map((key) => {
-        if (typeof $location.state[key] !== "object") return;
-        return $location.state[key];
-      });
+      imagesList = Object.keys($location?.state)
+        .map((key) => $location?.state[key])
+        .filter((data) => {
+          if (data?.id) return data;
+        });
     }
   });
+
+  function prevImage() {
+    if (Number($params?.imageid) > 1) {
+      navigate(
+        `/status/${$params?.feedid}/image/${Number($params.imageid) - 1}`,
+        { replace: true }
+      );
+    }
+  }
+
+  function nextImage() {
+    if (Number($params?.imageid) < imagesList.length) {
+      navigate(
+        `/status/${$params?.feedid}/image/${Number($params?.imageid) + 1}`,
+        { replace: true }
+      );
+    }
+  }
+
+  function closeImage() {
+    navigate(-1);
+  }
 </script>
 
 <div
@@ -34,10 +57,11 @@
   {/if}
 
   <!-- navigation images -->
-  {#if imagesList?.length > 2}
+  {#if imagesList && imagesList?.length > 1}
     <div class="absolute w-full px-5 flex justify-between text-white">
       <button
         class="w-10 h-10 flex justify-center items-center rounded-2xl bg-opacity-30 bg-gray-500"
+        on:click={prevImage}
       >
         <i>
           <svg
@@ -58,6 +82,7 @@
       </button>
       <button
         class="w-10 h-10 flex justify-center items-center rounded-2xl bg-opacity-30 bg-gray-500"
+        on:click={nextImage}
       >
         <i>
           <svg
@@ -78,4 +103,31 @@
       </button>
     </div>
   {/if}
+  <div
+    class="absolute w-full p-5 top-0 flex justify-between text-white bg-opacity-50 bg-black"
+  >
+    {#if imagesList}
+      <p class="w-3/4 line-clamp-1">
+        {imagesList[Number($params?.imageid) - 1]?.name}
+      </p>
+    {/if}
+    <button on:click={closeImage}>
+      <i>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </i>
+    </button>
+  </div>
 </div>
